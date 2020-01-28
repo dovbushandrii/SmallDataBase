b5c7ce49c98a65e75679a4a5165e175935a9d6a0
Andrey Dovbush <54791918+dovbushandrii@users.noreply.github.com>
2020-01-28 23:25:13 +0200

    Добавлен метод Adding()
    Полностью рабочий!
    И демо поиска(проверял фильтры)

--- DataBaseClasses.h
+++ DataBaseClasses.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <iostream>
 #include <string>
 using namespace std;
 enum ShowOptions {
@@ -18,17 +19,62 @@ enum SortBy {
 	DEPARTUREPOINT,
 	DESTINATION,
 	TYPE,
-	TIME,
+	DATE,
 	RATE
 };
+struct Date {
+	int day;
+	int month;
+	int year;
+	int hours;
+	int minutes;
+};
 struct TrainData {
-	int no;
+	string no;
 	string name;
 	string departurepoint;
 	string destination;
 	Type type;
+	Date date;
 	float rate;
 
+};
+
+class OnlineDataBase {
+public:
+	void loaddata(vector<TrainData> load) {
+		for (int i = 0; i < OnlineBase.size(); i++) {
+			OnlineBase.push_back(load[i]);
+		}
+	}
+	void addelement(TrainData add) {
+		OnlineBase.push_back(add);
+	}
+	void deletedata() {
+		for (int i = 0; i < OnlineBase.size(); i++) {
+			OnlineBase.pop_back();
+		}
+	}
+	void deleteelement(int i) {
+		if (i > OnlineBase.size()) {
+			throw exception("Индекс элемента для удаления больше кол-ва элементов базы");
+		}
+		else {
+			vector<TrainData> newbase;
+			for (int j = 0; j < i; j++) {
+				newbase.push_back(OnlineBase[j]);
+			}
+			for (int j = i + 1; j < OnlineBase.size(); j++) {
+				newbase.push_back(OnlineBase[j]);
+			}
+			OnlineBase = newbase;
+		}
+
+	}
+	vector<TrainData>& getdata() {
+		return OnlineBase;
+	}
+	vector<TrainData> OnlineBase;
 };
 
 class DataBaseInterface {
@@ -42,17 +88,33 @@ public:
 	void Menu(); // done
 private:
 	ShowOptions settings[6];   // Настройки отобрыжения данных  в поиске (Номер, Название, Направление, Тип, Дата, Рейтинг)
-
-	void Edit();
+	OnlineDataBase base;
+	void Edit(); // done
+	void Adding(); 
+	void Deleting(); 
 	void Settings(); // done
-	void Search();
+	void Search(); 
 	void Demo();
 	void Benchmark();
+	void traindatatooutstring(TrainData train); //done
+};
+
+class Adder {
+public:
+	void addnumber(TrainData& newtrain);
+	void addname(TrainData& newtrain);
+	void adddepdestination(TrainData& newtrain);
+	void addtype(TrainData& newtrain);
+	void adddate(TrainData& newtrain);
+	void addrate(TrainData& newtrain);
 };
 
 class Searcher {
 public: 
 	vector<TrainData> searchtrains(vector<TrainData>& base);
+	vector<TrainData> searchtrains(vector<TrainData>& base,string name);
+	vector<TrainData> searchtrains(vector<TrainData>& base,float rate);
+	vector<TrainData> searchtrains(vector<TrainData>& base,Date date);
 	void sortby(vector<TrainData>& SearchResult, SortBy filter);
 };
 
@@ -68,21 +130,3 @@ class Demonstrator {
 
 };
 
-class OnlineDataBase {
-public:
-	void loaddata(vector<TrainData> load) {
-		for (int i = 0; i < OnlineBase.size(); i++) {
-			OnlineBase.push_back(load[i]);
-		}
-	}
-	void deletedata() {
-		for (int i = 0; i < OnlineBase.size(); i++) {
-			OnlineBase.pop_back();
-		}
-	}
-	vector<TrainData>& getdata() {
-		return OnlineBase;
-	}
-private:
-	vector<TrainData> OnlineBase;
-};
